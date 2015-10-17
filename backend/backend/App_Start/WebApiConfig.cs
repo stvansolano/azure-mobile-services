af9b418cf56ec0c15aba9a64865689aa7346e25a
stvansolano <stvansolano@gmail.com>
2015-10-17 14:37:50 -0600

    Added ToDoExample for WebAPI from scratch

---
+++ backend/backend/App_Start/WebApiConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Web.Http;
+using Backend.DataObjects;
+using Backend.Models;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace Backend
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            new MobileAppConfiguration()
+                .UseDefaultConfiguration()
+                .ApplyTo(config);
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
+            Database.SetInitializer(new MobileServiceInitializer());
+
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+
+    public class MobileServiceInitializer : DropCreateDatabaseIfModelChanges<MobileServiceContext>
+    {
+        protected override void Seed(MobileServiceContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
